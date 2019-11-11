d29f8c58c1a81a54619100a44a4dda2010e3d77f
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-11-10 19:04:55 -0500

    Add readFile to test that file is read properly

--- airlinePriorityQueue.c
+++ airlinePriorityQueue.c
@@ -1,3 +1,7 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+
 struct passenger
 {
     char name[50];
@@ -7,8 +11,22 @@ struct passenger
     int priorityNum;
 };
 
+void readFile()
+{
+    char str[10000];
+    FILE * file;
+    file = fopen("customers.txt", "r");
+    if(file)
+    {
+        while(fscanf(file, "%s", str)!=EOF)
+            printf("%s\n", str);
+        fclose(file);
+    }
+}
+
+
 int main()
 {
-
+    readFile();
     return 0;
 }
